1ffc38806fd269644940738bf4528fb667540cb8
Shawn Carpenter <shawn.carpenter@gmail.com>
2020-11-12 14:41:29 -0800

    sockets testing

--- src/components/app/test-components/SocketComponent.jsx
+++ src/components/app/test-components/SocketComponent.jsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 
 export default function App() {
-  return <h1>Hello Socket sandbox</h1>;
+  
+  const [socket, setSocket] = useState(socketIOClient('http://localhost:7890'));
+  const [songUrl, setSongUrl] = useState('');
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    if(!socket) return;
+    socket.on('songEvent', song => {
+      setSongUrl(song.newSong);
+    });
+    socket.on('pause', () => {
+      setPlaying(false);
+    });
+    socket.on('play', () => {
+      setPlaying(true);
+    });
+    
+  }, [socket]);
+  
+  
+
+
+  const handleClick = () => {
+    socket.emit('setSongUrl', songUrl);
+  };
+
+  const handlePlayPause = () => {
+   
+    socket.emit('playChange', playing);
+  };
+  const handleChange = ({ target }) => {
+    setSongUrl(target.value);
+  };
+  return (
+    <div>
+      <input 
+        name="songUrl"
+        value={songUrl}
+        onChange={handleChange}/>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={handlePlayPause}>{playing ? '||' : '>'}</button>
+    </div>
+  );
 }
