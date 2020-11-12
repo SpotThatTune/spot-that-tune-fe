309a3699082f949d315610efc3dd1af8157d864e
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-11-12 11:24:46 -0800

    Landing page.jsx component finished

---
+++ src/components/landing/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../../public/assets/spot-that-tune-logo.png';
+export const LandingPage = () => {
+    
+  return (
+    <>
+      <header>
+        <h1>SPOT THAT TUNE</h1>
+      </header>
+      <div className="logo">
+        <img src={logo} alt="logo"/>
+      </div>
+                
+    </>
+  );
+    
+};
+
+export default LandingPage;
---
+++ src/components/spotifyauth/SpotifyOAuth.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+export const SpotifyOAuth = () => {
+    
+  return (
+    <>
+      
+                
+    </>
+  );
+    
+};
+
+export default SpotifyOAuth;
