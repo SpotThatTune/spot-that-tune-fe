008a087074c8ed1ff141a64fa9cc31f822decc2f
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-11-12 16:53:05 -0800

    audio working

--- src/components/spotifyauth/SpotifyOAuth.jsx
+++ src/components/spotifyauth/SpotifyOAuth.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import styles from './SpotifyOAuth.css';
 
 export const SpotifyOAuth = () => {
-    
+  const authEndpoint = 'https://accounts.spotify.com/authorize';
+  const clientId = process.env.CLIENT_ID;
+  const redirectUri = process.env.REDIRECT_URI;
+  const scopes = [
+    'streaming',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+    'user-read-playback-state',
+  ];
   return (
     <div className={styles.login}>
-      <button className={styles.button}>
+      <button className={styles.button} 
+      // eslint-disable-next-line max-len
+        onClick={() => window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`}>
         Login to Spotify
         <div className={styles.button__horizontal}></div>
         <div className={styles.button__vertical}></div>
