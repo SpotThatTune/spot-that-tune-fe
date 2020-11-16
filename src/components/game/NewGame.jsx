f94c41de81a78f89f41383136fc4b3354ab154a1
Shawn Carpenter <shawn.carpenter@gmail.com>
2020-11-16 14:05:46 -0800

    getting playlists into state

--- src/components/game/NewGame.jsx
+++ src/components/game/NewGame.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchUserPlaylists, setToken } from '../../actions/SpotifyActions';
+
 
 const NewGame = () => {
+  const userPlaylists = useSelector(state => state.userPlaylists);
+  const token = useSelector(state => state.token);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    if(!token) {
+      const hash = window.location.hash;
+      const params = hash.split('&');
+      const access_token = params[0].slice(params[0].indexOf('=') + 1);
+      console.log(access_token);
+      dispatch(setToken(access_token));
+      dispatch(fetchUserPlaylists(access_token));  
+    }
+    
+
+  }, []);
   return (
     <div>
       New Game page
