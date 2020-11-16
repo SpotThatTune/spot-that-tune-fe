c5f03335e68edfb2566105cf49d0094355661e68
Shawn Carpenter <shawn.carpenter@gmail.com>
2020-11-16 10:06:58 -0800

    added about, instructions, and game pages

---
+++ src/components/game/NewGame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewGame = () => {
+  return (
+    <div>
+      New Game page
+    </div>
+  );
+};
+
+export default NewGame;
---
+++ src/components/instructions/Instructions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Instructions = () => {
+  return (
+    <div>
+      <h1>How to play</h1>
+    </div>
+  );
+};
+
+export default Instructions;
