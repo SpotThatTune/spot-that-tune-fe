a04c0526a438284c90f2e714ed99c0a2624189dc
Shawn Carpenter <shawn.carpenter@gmail.com>
2020-11-19 15:19:54 -0800

    added scoring (#28)

--- src/components/game/Host.jsx
+++ src/components/game/Host.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   setCurrentTrack,
@@ -14,16 +14,31 @@ export const Host = () => {
     game,
     playing,
   } = useSelector(state => state);
-    
+  const [guess, setGuess] = useState(''); 
+  const [player, setPlayer] = useState({}); 
   const dispatch = useDispatch();
   useEffect(() => {
     if(!socket) return;
     socket.on('CHANGE_TRACK', track => {
       dispatch(setCurrentTrack(track));
     });
-      
+    socket.on('PAUSE', () => {
+      const audioEl = document.getElementById('player');
+      audioEl.pause();
+      dispatch(setPlaying(false));
+      console.log('Hit');
+    });
+    socket.on('PLAY', () => {
+      const audioEl = document.getElementById('player');
+      audioEl.play();
+      dispatch(setPlaying(true));
+    }); 
     socket.on('GAME_INFO', ({ game }) => {
       dispatch(setGame(game));
+    });
+    socket.on('GUESS', ({ userGuess, user, playerId }) => {
+      setGuess(userGuess);
+      setPlayer({ name:user, id:playerId });
     });
   }, [socket]);
 
@@ -37,9 +52,19 @@ export const Host = () => {
   const handlePlayPause = () => {
     socket.emit('PLAY_CHANGE', playing, game.id);  
   };
+
+  const handleCorrect = () => {
+    socket.emit('CORRECT', { playerId:player.id,
+      gameId:game.id });
+  };
+
+  const handleIncorrect = () => {
+    socket.emit('INCORRECT');
+  };
     
   return (
     <div>
+      <h3>{currentTrack.name ? currentTrack.name : 'Select a track'}</h3>
       <button onClick={handleClick}>New Song</button>
       <button
         onClick={handlePlayPause}>{playing ? '||' : '>'}</button> 
@@ -48,6 +73,15 @@ export const Host = () => {
         className="player"
         controls={true} 
         src={currentTrack.preview_url}></audio>
+      <h3>{player.name} guessed {guess}</h3>
+      <button
+        name="correct"
+        onClick={handleCorrect}
+      >Correct</button>
+      <button
+        name="inCorrect"
+        onClick={handleIncorrect}
+      >Incorrect</button>
     </div>
   );
 };
