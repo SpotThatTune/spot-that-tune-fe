920c48aa0d84799ef83c78996448e4dd01feb07c
Edgar Cuellar <66703437+EdgarPDX@users.noreply.github.com>
2020-11-19 19:49:51 -0800

    Feature/styling game page (#33)
    * styling new Game Page
    
    * more css and created headercomponent
    
    * added header component to Game page
    
    * styled scoreboard

--- src/components/game/ScoreBoard.jsx
+++ src/components/game/ScoreBoard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import styles from './ScoreBoard.css';
 
 const ScoreBoard = () => {
   const game = useSelector(state => state.game);
@@ -9,14 +10,18 @@ const ScoreBoard = () => {
     return b.score - a.score;
   }).map(player => (
     <li key={player.id}>
-    Name {player.user} 
-    Score: {player.score}</li>));
+      <span>{player.user}:</span> 
+      <span> {player.score} points</span>
+    </li>));
  
   return (
-    <div>
-      <h3>Round {game.round} of 5</h3>
-      <h3>Scores:</h3>
-      <ol>{scores}</ol>
+    <div className={styles.scoreboard}>
+      <h3 className={styles.rounds}>Round {game.round} of 5</h3>
+      <div className={styles.scores}>
+        <h3>SCOREBOARD:</h3>
+        <ol>{scores}</ol>
+      </div>
+      
     </div>
   );
 };
